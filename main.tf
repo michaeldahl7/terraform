a7c6ef7f6cc295355fc15597be54c821cf3303c8
Michael Dahl <michaeldahl7@gmail.com>
2023-01-26 10:29:57 -0800

    initial commit

---
+++ main.tf
@@ -0,0 +1,99 @@
+resource "aws_vpc" "dahl_vpc" {
+  cidr_block           = "10.123.0.0/16"
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "dev"
+  }
+}
+
+resource "aws_subnet" "dahl_public_subnet" {
+  vpc_id                  = aws_vpc.dahl_vpc.id
+  cidr_block              = "10.123.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "us-west-1a"
+
+  tags = {
+    Name = "dahl-public"
+  }
+}
+
+resource "aws_internet_gateway" "dahl_internet_gateway" {
+  vpc_id = aws_vpc.dahl_vpc.id
+
+  tags = {
+    Name = "dahl-igw"
+  }
+}
+
+resource "aws_route_table" "dahl_public_rt" {
+  vpc_id = aws_vpc.dahl_vpc.id
+
+  tags = {
+    Name = "dahl-public-rt"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.dahl_public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.dahl_internet_gateway.id
+}
+
+resource "aws_route_table_association" "dahl_public_assoc" {
+  subnet_id      = aws_subnet.dahl_public_subnet.id
+  route_table_id = aws_route_table.dahl_public_rt.id
+}
+
+
+resource "aws_security_group" "dahl_sg" {
+  name        = "dahl_sg"
+  description = "Dahl security group"
+  vpc_id      = aws_vpc.dahl_vpc.id
+
+  ingress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["68.5.233.155/32"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_key_pair" "dahl_auth" {
+  key_name   = "dahlkey"
+  public_key = file("~/.ssh/dahlkey.pub")
+}
+
+resource "aws_instance" "dev_node" {
+  ami                    = data.aws_ami.server_ami.id
+  instance_type          = "t2.micro"
+  key_name               = aws_key_pair.dahl_auth.id
+  vpc_security_group_ids = [aws_security_group.dahl_sg.id]
+  subnet_id              = aws_subnet.dahl_public_subnet.id
+  user_data              = file("userdata.tpl")
+
+  root_block_device {
+    volume_size = 10
+  }
+
+  tags = {
+    Name = "dev-node"
+  }
+
+  provisioner "local-exec" {
+    command = templatefile("${var.host_os}-ssh-config.tpl", {
+      hostname     = self.public_ip,
+      user         = "ubuntu",
+      identityfile = "~/.ssh/dahlkey"
+    })
+    interpreter = ["bash", "-c"]
+  }
+
+}
